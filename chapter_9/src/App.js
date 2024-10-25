227b6cac9d69dedbb0084d95b223051aad9f4c16
Michael van den Berg <michaelmarkus115@gmail.com>
2024-10-25 09:39:44 +0200

    commit

---
+++ chapter_9/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+//firebase dependancies
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1></h1>
+      </div>
+    );
+  }
+}
+export default App;
